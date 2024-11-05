236cd2c2fb2c575624faa0f828994232f8572cbe
Janick Lehmann <j.clehmann@hotmail.com>
2024-11-05 23:19:55 +0100

    Updates to login form

--- src/public/partials/azure-sso-public-login-form.php
+++ src/public/partials/azure-sso-public-login-form.php
@@ -14,7 +14,9 @@
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
+<hr>
 <div class="azure-sso-wrap">
+    <p>or sign in using single-sign-on</p> <!-- TODO: localize -->
     <a id="azure-sso-login-button" class="button button-large" href="<?php echo esc_html($login_url) ?>" >
         <svg xmlns="http://www.w3.org/2000/svg" class="icon" fill="currentColor" viewBox="0 0 448 512">
             <path d="M0 32h214.6v214.6H0V32zm233.4 0H448v214.6H233.4V32zM0 265.4h214.6V480H0V265.4zm233.4 0H448V480H233.4V265.4z"/>
